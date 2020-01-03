b0ce543a70899a79c5cff3aeddb514fec12362d1
AdibHoque <46087857+AdibHoque@users.noreply.github.com>
2020-01-03 21:52:41 +0600

    Update Electro.py

--- Electro.py
+++ Electro.py
@@ -1213,7 +1213,7 @@ async def customrole(ctx, user: discord.Member, *,price: str):
         embed.add_field(name="Purchased by:", value="{}".format(user))
         embed.add_field(name="Item:", value="Custom Role")
         embed.add_field(name="Price:", value="{}".format(price))
-	await bot.say(embed=embed)
+ await bot.say(embed=embed)
         
 @bot.command(pass_context=True)
 @commands.has_permissions(administrator=True)
