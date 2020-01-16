8dbc79759b94765ad8cca142df52e07b86ba9661
AdibHoque <46087857+AdibHoque@users.noreply.github.com>
2020-01-16 22:27:15 +0600

    Update Electro.py

--- Electro.py
+++ Electro.py
@@ -597,18 +597,18 @@ async def plsboi(ctx):
 async def help(ctx):
     author = ctx.message.author
     r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
-    embed = discord.Embed(description='To see a detailed page, just react with their respective emojis or a add the page number after the `e!help` command. Like this `e!help1`, `e!help2` Etc.', color = 0xFFBF00) 
-    embed.set_author(name='ELECTRO HELP MENU', icon_url='https://cdn.discordapp.com/attachments/656517276832366595/656519678499487745/ELECTRO.png')
+    embed = discord.Embed(description='To see a detailed page, just react with their numeral emojis or add the page number after the `e!help` command. E.G.`e!help1`, `e!help2` Etc.', color = 0xFFBF00) 
+    embed.set_author(name='[ELECTRO HELP MENU](https://discord.gg/kuWVFpR)', icon_url='https://cdn.discordapp.com/attachments/656517276832366595/656519678499487745/ELECTRO.png')
     embed.set_image(url = 'https://cdn.discordapp.com/attachments/656517276832366595/656760631474520074/ELECTRO_ELECTRIFY_YOUR_SERVER.gif')
     embed.add_field(name = '<:ElectroGeneral:666202936929681418> General Commands - (8)',value ='`ping`,`userinfo`,`serverinfo`,`ownerinfo`,`avatar`,`membercount`,`invite`,`upvote`',inline = False)
     embed.add_field(name = '<:ElectroModeration:666920202818027531> Moderation Commands - (13)',value ='`kick`,`ban`,`setnick`,`role`,`say`,`DM`,`english`,`rolecolor`,`lockdown`,`unlock`,`menro`,`mute`,`unmute`',inline = False)
     embed.add_field(name = '<:ElectroFun:666203658467147776> Fun Commands - (16)',value ='`meme`,`joke`,`love`,`slap`, `kiss`, `hug`, `virgin`, `gender`, `tweet`, `rolldice`, `flipcoin`, `howgay`, `whowouldwin`, `captcha`,`magik`,`deepfry`',inline = False)
-    embed.add_field(name = '<:ElectroMusic:666203904186515467> Music Commands - (8)',value ='`play`,`skip`,`stop`,`NP`,`queue`,`pause`,`resume`,`volume`\n\n[Add Bot](https://discordapp.com/api/oauth2/authorize?client_id=629323586930212884&permissions=8&scope=bot) | [Join Server](https://discord.gg/kuWVFpR ) | [Upvote](https://discordbots.org/bot/629323586930212884/vote)',inline = False)
+    embed.add_field(name = '<:ElectroMusic:666203904186515467> Music Commands - (8)',value ='`play`,`skip`,`stop`,`NP`,`queue`,`pause`,`resume`,`volume`\n**Additional Links:**\n[Add Bot](https://discordapp.com/api/oauth2/authorize?client_id=629323586930212884&permissions=8&scope=bot) | [Join Server](https://discord.gg/kuWVFpR ) | [Upvote](https://discordbots.org/bot/629323586930212884/vote)',inline = False)
     msg = await bot.send_message(author ,embed=embed)
-    await bot.add_reaction(msg,':ElectroGeneral:666202936929681418')
-    await bot.add_reaction(msg,':ElectroModeration:666920202818027531')
-    await bot.add_reaction(msg,':ElectroFun:666203658467147776')
-    await bot.add_reaction(msg,':ElectroMusic:666203904186515467')
+    await bot.add_reaction(msg,'1⃣')
+    await bot.add_reaction(msg,'2⃣')
+    await bot.add_reaction(msg,'3⃣')
+    await bot.add_reaction(msg,'4⃣')
     await bot.say('📨 Check Your DMs For Bot Commands!')
     			
 @bot.command(pass_context = True)
@@ -856,7 +856,7 @@ async def on_message_edit(before, after):
 @bot.event
 async def on_reaction_add(reaction, user):
 	if reaction.message.server is None:
-		if reaction.emoji == '<:ElectroGeneral:666202936929681418>':
+		if reaction.emoji == '1⃣':
 			embed = discord.Embed(title = 'GENERAL COMMANDS', color = 0xFFBF00)
 			embed.set_image(url = 'https://cdn.discordapp.com/attachments/656517276832366595/656760777973432330/ELECTRO_HELP1.gif')
 			embed.add_field(name = 'Ping',value ='Returns ping lantency!\n**USAGE:**``e!ping``',inline = False)
@@ -869,7 +869,7 @@ async def on_reaction_add(reaction, user):
 			embed.add_field(name = 'Upvote',value ='Sends bot upvote link!\n**USAGE:**``e!upvote``',inline = False)
 			embed.add_field(name = 'Emoji',value ='Sends url of the emoji!\n**USAGE:**``e!emoji :emoji: ``\n[Add Bot](https://discordapp.com/api/oauth2/authorize?client_id=629323586930212884&permissions=8&scope=bot) | [Join Server](https://discord.gg/kuWVFpR ) | [Upvote](https://discordbots.org/bot/629323586930212884/vote)',inline = False)
 			await bot.send_message(reaction.message.channel, embed=embed)
-		if reaction.emoji == '<:ElectroModeration:666920202818027531>':
+		if reaction.emoji == '2⃣':
 			embed = discord.Embed(title = 'MODERATION COMMANDS', color = 0xFFBF00)
 			embed.set_image(url = 'https://cdn.discordapp.com/attachments/656517276832366595/656760817852874752/ELECTRO_HELP2.gif')
 			embed.add_field(name = 'Kick',value ='Kicks out mentioned user from the server!\n**USAGE:**``e!kick @user``',inline = False)
