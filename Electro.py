dac67a03fdfe9738ad1e6ff21cc8e2a2e3c9d396
AdibHoque <46087857+AdibHoque@users.noreply.github.com>
2020-01-20 14:18:26 +0600

    Update Electro.py

--- Electro.py
+++ Electro.py
@@ -773,7 +773,7 @@ async def embed(ctx, channel: discord.Channel=None, *, msg: str):
 @commands.has_permissions(administrator=True) 
 async def announce(ctx, channel: discord.Channel=None, *, msg: str):
 	embed=discord.Embed(title="ANNOUNCEMENT", description="{}".format(msg), color=0xFFBF00)
-	embed.set_author(name="{}".format(ctx.message.author))
+	embed.set_author(name="{}".format(ctx.message.author), icon_url=ctx.message.author.avatar_url)
 	embed.set_thumbnail(url=ctx.message.server.icon_url)
 	embed.timestamp = datetime.datetime.utcnow()
 	await bot.send_message(channel, embed=embed)
