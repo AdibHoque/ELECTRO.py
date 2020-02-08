9c2fc575128221c932f8b98d9f78051622521390
Adib Hoque <adibhoque04@gmail.com>
2020-02-08 22:28:51 +0600

    Update Electro.py

--- Electro.py
+++ Electro.py
@@ -1452,5 +1452,19 @@ def _save():
 @bot.command()
 async def save():
     _save()
-            
+     
+@bot.command()
+async def whosthatpokemon(ctx):
+    num = random.randint(1, 926)
+    async with aiohttp.ClientSession().get(f'https://pokeapi.co/api/v2/pokemon-form/{num}/') as resp:
+        data = await resp.json()
+	embed = discord.Embed(title='Who\'s that pokemon?', color=0xFFBF00)
+	embed.set_image(url=data['sprites']['front_default'])
+	await bot.send(embed=embed)
+	guess = await bot.wait_for_message(timeout= 30, author=ctx.message.author, channel=ctx.message.channel)
+	if guess.content == data['name']:
+		await bot.say(f'Correct! That pokemon is {data["name"]}')
+	else:
+		await bot.say(f'Incorrect! That pokemon is {data["name"]}') 
+       
 bot.run(os.getenv('Token'))
