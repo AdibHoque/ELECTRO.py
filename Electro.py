6eced477edc2400e38d6129074166ef52c8b88a3
Adib Hoque <adibhoque04@gmail.com>
2020-02-09 14:39:13 +0600

    Update Electro.py

--- Electro.py
+++ Electro.py
@@ -50,7 +50,7 @@ async def on_command_error(error, ctx):
     elif isinstance(error, commands.MissingRequiredArgument):
         embed=discord.Embed(description="<a:ElectroError:646994154152525845> **Missing Required Argument.**\nPlease type `e!help` or join [Support Server](https://discord.gg/kuWVFpR) to know how to use this command properly!", color=0xFFBF00)
         await bot.send_message(ctx.message.channel, embed=embed)
-    elif isinstance(error, commands.MissingPermissions):
+    elif isinstance(error, MissingPermissions):
         embed=discord.Embed(description="<a:ElectroError:646994154152525845> You need the `{}` permission use this command!".format(error.missing_perms), color=0xFFBF00)
         await bot.send_message(ctx.message.channel, embed=embed)
     elif isinstance(error, BotMissingPermissions):
