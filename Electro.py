24b8522505efb23ec65d8176442e1c6fd2d4f02d
Adib Hoque <adibhoque04@gmail.com>
2020-02-18 16:58:36 +0600

    Update Electro.py

--- Electro.py
+++ Electro.py
@@ -563,13 +563,13 @@ async def love(ctx, user: discord.Member = None, *, user2: discord.Member = None
             await bot.say(embed=embed)
  
 @bot.command(pass_context=True, aliases=['gay','gayrate','howmuchgay','g8','lesbian'])
-async def howgay(ctx, user: discord.Member):
+async def howgay(ctx, user: discord.Member=None):
         if user.id == '496978159724396545':
                 embed = discord.Embed(title="Gayrate of ADIB HOQUE#2212",description="[□□□□□□□□□□ ](https://discord.gg/kuWVFpR)(**0%**)", color=0xFFBF00)
                 embed.set_footer(text="Im straighter than the pole that your mom 💃dances on - ADIB", icon_url=ctx.message.author.avatar_url)
                 await bot.say(embed=embed)
-                Return
-        if user == None:
+        Return
+        if user is None:
                 score = random.randint(0, 100)
                 filled_progbar = round(score / 100 * 10)
                 counter_ = '■' * filled_progbar + '□' * (10 - filled_progbar)
