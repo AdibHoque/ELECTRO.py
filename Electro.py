7cb442b3511be8b08679f35a082e839a59c3bcf9
Adib Hoque <adibhoque04@gmail.com>
2020-03-09 14:35:59 +0600

    Update Electro.py

--- Electro.py
+++ Electro.py
@@ -596,75 +596,6 @@ async def rolldice(ctx):
     choices = ['1', '2', '3', '4', '5', '6']
     em = discord.Embed(title='Rolled! (1 6-sided dice)', description='{}'.format(random.choice(choices)),color=0xFFBF00)
     await bot.say(embed=em)
-    
-@bot.command(pass_context=True)
-async def kiss(ctx, user: discord.Member):
-    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
-    randomurl = ["https://media3.giphy.com/media/G3va31oEEnIkM/giphy.gif", "https://i.imgur.com/eisk88U.gif", "https://media1.tenor.com/images/e4fcb11bc3f6585ecc70276cc325aa1c/tenor.gif?itemid=7386341", "http://25.media.tumblr.com/6a0377e5cab1c8695f8f115b756187a8/tumblr_msbc5kC6uD1s9g6xgo1_500.gif"]
-    if user.id == ctx.message.author.id:
-        await bot.say("Goodluck kissing yourself {}".format(ctx.message.author.mention))
-    else:
-        embed = discord.Embed(title=f"{user.name} You just got a kiss from {ctx.message.author.name}", color = 0xFFBF00)
-        embed.set_image(url=random.choice(randomurl))
-        await bot.say(embed=embed)
-
-@bot.command(pass_context=True)
-async def hug(ctx, user: discord.Member):
-    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
-    if user.id == ctx.message.author.id:
-        await bot.say("{} You can't hug yourself!😒".format(user.mention))
-    else:
-        randomurl = ["http://gifimage.net/wp-content/uploads/2017/09/anime-hug-gif-5.gif", "https://media1.tenor.com/images/595f89fa0ea06a5e3d7ddd00e920a5bb/tenor.gif?itemid=7919037", "https://media.giphy.com/media/NvkwNVuHdLRSw/giphy.gif"]
-        embed = discord.Embed(title=f"{user.name} You just got a hug from {ctx.message.author.name}", color = 0xFFBF00)
-        embed.set_image(url=random.choice(randomurl))
-        await bot.say(embed=embed)
-
-@bot.command(pass_context=True)
-async def gender(ctx, user: discord.Member):
-    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
-    random.seed(user.id)
-    genderized = ["Male", "Female", "Transgender", "Unknown", "Can't be detected", "Shemale"]
-    randomizer = random.choice(genderized)
-    if user == ctx.message.author:
-        embed = discord.Embed(title="You should know your own gender😒", color = 0xFFBF00)
-        await bot.say(embed=embed)
-    else:
-        embed = discord.Embed(color=0xFFBF00)
-        embed.add_field(name=f"{user.name}'s gender check results", value=f"{randomizer}")
-        await bot.say(embed=embed)
-
-@bot.command(pass_context=True)
-async def virgin(ctx, user: discord.Member):
-    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
-    random.seed(user.id)
-    results= ["Not a virgin", "Never been a virgin", "100% Virgin", "Half virgin :thinking:", "Forever Virgin"]
-    randomizer = random.choice(results)
-    if user == ctx.message.author:
-        embed = discord.Embed(title="Go ask yourself if you are still a virgin or not!", color = discord.Color((r << 16) + (g << 8) + b))
-        await bot.say(embed=embed)
-    else:
-        embed = discord.Embed(color=0xFFBF00)
-        embed.add_field(name=f"{user.name}'s virginity check results", value=f"{randomizer}")
-        await bot.say(embed=embed)
-
-@bot.command(pass_context=True)
-async def joke(ctx):
-    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
-    joke = ["What do you call a frozen dog?\nA pupsicle", "What do you call a dog magician?\nA labracadabrador", "What do you call a large dog that meditates?\nAware wolf", "How did the little scottish dog feel when he saw a monster\nTerrier-fied!", "Why did the computer show up at work late?\nBecause it had a hard drive", "Autocorrect has become my worst enime", "What do you call an IPhone that isn't kidding around\nDead Siri-ous", "The guy who invented auto-correct for smartphones passed away today\nRestaurant in peace", "You know you're texting too much when you say LOL in real life, instead of laughing", "I have a question = I have 18 Questions\nI'll look into it = I've already forgotten about it", "Knock Knock!\nWho's there?\Owls say\nOwls say who?\nYes they do.", "Knock Knock!\nWho's there?\nWill\nWill who?\nWill you just open the door already?", "Knock Knock!\nWho's there?\nAlpaca\nAlpaca who?\nAlpaca the suitcase, you load up the car.","Once a guy foumd a genie lamp, He rubbed the lamp and wished that he dosen't wants to die virgin. Then the genie granted him immortality!'", "Yo momma's teeth is so yellow, when she smiled at traffic, it slowed down.", "Yo momma's so fat, she brought a spoon to the super bowl.", "Yo momma's so fat, when she went to the beach, all the whales started singing 'We are family'", "Yo momma's so stupid, she put lipstick on her forehead to make up her mind.", "Yo momma's so fat, even Dora can't explore her.", "Yo momma's so old, her breast milk is actually powder", "Yo momma's so fat, she has to wear six different watches: one for each time zone", "Yo momma's so dumb, she went to the dentist to get a bluetooth", "Yo momma's so fat, the aliens call her 'the mothership'", "Yo momma's so ugly, she made an onion cry.", "Yo momma's so fat, the only letters she knows in the alphabet are K.F.C", "Yo momma's so ugly, she threw a boomerang and it refused to come back", "Yo momma's so fat, Donald trump used her as a wall", "Sends a cringey joke\nTypes LOL\nFace in real life : Serious AF", "I just got fired from my job at the keyboard factory. They told me I wasn't putting enough shifts.", "Thanks to autocorrect, 1 in 5 children will be getting a visit from Satan this Christmas.", "Have you ever heard about the new restaurant called karma?\nThere's no menu, You get what you deserve.", "Did you hear about the claustrophobic astronaut?\nHe just needed a little space", "Why don't scientists trust atoms?\nBecase they make up everything", "How did you drown a hipster?\nThrow him in the mainstream", "How does moses make tea?\nHe brews", "A man tells his doctor\n'DOC, HELP ME. I'm addicted to twitter!'\nThe doctor replies\n'Sorry i don't follow you...'", "I told my wife she was drawing her eyebrows too high. She looked surprised.", "I threw a boomeranga a few years ago. I now live in constant fear"]
-    embed = discord.Embed(color = 0xFFBF00)
-    embed.add_field(name=f"Here is a random joke {ctx.message.author}!", value=random.choice(joke))
-    await bot.say(embed=embed)
-
-@bot.command(pass_context=True)
-async def slap(ctx, user: discord.Member = None):
-    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
-    gifs = ["http://rs20.pbsrc.com/albums/b217/strangething/flurry-of-blows.gif?w=280&h=210&fit=crop", "https://media.giphy.com/media/LB1kIoSRFTC2Q/giphy.gif", "https://i.imgur.com/4MQkDKm.gif"]
-    if user == None:
-        await bot.say(f"{ctx.message.author.mention} Please mention a user to slap!")
-    else:
-        embed = discord.Embed(title=f"{ctx.message.author.name} Just slapped the shit out of {user.name}!", color = 0xFFBF00)
-        embed.set_image(url=random.choice(gifs))
-        await bot.say(embed=embed)
 
 @bot.command(pass_context=True)
 async def membercount(ctx, *args):
@@ -1702,15 +1633,15 @@ async def urban(ctx, *, msg:str):
                                 await bot.say(embed=embed)
 
 @bot.command(pass_context=True)
-async def nekosapi(ctx, user: discord.Member):
+async def hug(ctx, user: discord.Member):
         if user.id == ctx.message.author.id:
-                await bot.say('Go infront of a mirror and  yourself!')
+                await bot.say('Go infront of a mirror and hug yourself!')
         else:
                 url = f"https://nekos.life/api/v2/img/hug"
                 async with aiohttp.ClientSession() as cs:
                                async with cs.get(url) as r:
                                 res = await r.json()
-                                embed = discord.Embed(title = '{} Just got a hug from {}'.format(user, ctx.message.author),color = 0xFFBF00)
+                                embed = discord.Embed(title = '{} Just hugged {}'.format(ctx.message.author, user),color = 0xFFBF00)
                                 embed.set_image(url=res['url'])
                                 await bot.say(embed=embed)
 
@@ -1734,7 +1665,7 @@ async def divide(ctx, a: int, b:int):
 async def pokefuse(ctx):
         num = random.randint(1, 151)
         num2 = random.randint(1, 150)
-        embed = discord.Embed(title="pokefusion", color=0xFFBF00)
+        embed = discord.Embed(title="Pokefusion", color=0xFFBF00)
         embed.set_image(url="http://images.alexonsager.net/pokemon/fused/{}/{}.{}.png".format(num, num, num2))
         await bot.send_message(ctx.message.channel, embed=embed)
        
