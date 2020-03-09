feda3420f72f8baedf261c1be6c62c47a94d4924
Adib Hoque <adibhoque04@gmail.com>
2020-03-09 14:52:36 +0600

    Update Electro.py

--- Electro.py
+++ Electro.py
@@ -1712,7 +1712,7 @@ async def cuddle(ctx, user: discord.Member):
 
 @bot.command(pass_context=True)
 async def 8ball(ctx, *, question:str):
-        if question == None:
+        if question is None:
                 await bot.say('`e!8ball <question>')
         else:
                 url = f"https://nekos.life/api/v2/img/8ball"
@@ -1722,7 +1722,6 @@ async def 8ball(ctx, *, question:str):
                                 embed = discord.Embed(title = '{}?'.format(question),color = 0xFFBF00)
                                 embed.set_image(url=res['url'])
                                 await bot.say(embed=embed)
- 
 
 @bot.command(pass_context=True, aliases=['+'])
 async def add(ctx, a: int, b:int):
