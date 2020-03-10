b757cef39e35335893b6fceb22021ed7789e0bcd
Adib Hoque <adibhoque04@gmail.com>
2020-03-10 18:15:27 +0600

    Update Electro.py

--- Electro.py
+++ Electro.py
@@ -1384,11 +1384,11 @@ async def phcomment(ctx, *, msg:str):
         url = f"https://nekobot.xyz/api/imagegen?type=phcomment&image={ctx.message.author.avatar_url}&text={msg}&username={ctx.message.author.name}" 
         async with aiohttp.ClientSession() as cs:
                 async with cs.get(f"https://nekobot.xyz/api/imagegen?type=phcomment"
-                              f"&image={ctx.message.author.avatar_url}"
-                              f"&text={msg}&username={ctx.message.author.name}") as r:
+                                  f"&image={ctx.message.author.avatar_url}"
+                                  f"&text={msg}&username={ctx.message.author.name}") as r:
                         res = await r.json()
                         embed = discord.Embed(color=0xFFBF00)
-                        embed.set_image(url=res[0]['message'])
+                        embed.set_image(url=res['message'])
                         await bot.say(embed=embed)
 				 
 @bot.command(pass_context = True)
