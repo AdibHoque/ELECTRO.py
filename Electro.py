76536a3b6ac8441ad7cb902fbb0048f102b0d5ed
Adib Hoque <adibhoque04@gmail.com>
2020-05-12 05:09:58 +0600

    Update Electro.py

--- Electro.py
+++ Electro.py
@@ -51,6 +51,7 @@ async def is_nsfw(channel: discord.Channel):
 @bot.event
 async def on_ready():
     print('ONLINE')
+    print(len(bot.users))
     bot.loop.create_task(status_task())
     global amounts
     try:
