0160f2b17208ebd654c1a0f82b9518c961f1008b
Adib Hoque <adibhoque04@gmail.com>
2020-05-27 20:19:04 +0600

    Update Electro.py

--- Electro.py
+++ Electro.py
@@ -34,9 +34,9 @@ def is_premium(ctx):
 async def status_task():
     while True:
         m = len(set(bot.get_all_members()))/1000
-        await bot.change_presence(game=discord.Game(name='e!help • {}K Users'.format(m)))
+        await bot.change_presence(game=discord.Game(name='Help → e!help | {}K Users'.format(m)))
         await asyncio.sleep(12)
-        await bot.change_presence(game=discord.Game(name='e!help • '+str(len(bot.servers))+' Guilds'))
+        await bot.change_presence(game=discord.Game(name='Help → e!help | '+str(len(bot.servers))+' Guilds'))
         await asyncio.sleep(12)
 
 async def is_nsfw(channel: discord.Channel):
